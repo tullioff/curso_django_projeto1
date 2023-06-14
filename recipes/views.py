48a08b07e3beff66b2e18d5c313a616453036e15
tullioff <tulliofariasb@gmail.com>
2023-06-13 22:59:31 -0300

    Muita coisa até a aula 46

---
+++ recipes/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'recipes/pages/home.html')
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-content.html')
