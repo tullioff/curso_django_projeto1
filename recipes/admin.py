a5f1b91060095fabeef70a79113fb7558cbeee54
tullioff <tulliofariasb@gmail.com>
2023-07-03 08:33:53 -0300

    commit da aula 58. criação de models e outros detalhes.

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from recipes.models import Category, Recipe
 
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Recipe, RecipeAdmin)
--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-
+from utils.recipes.factory_faker import make_recipe
+from .models import Recipe
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    recipes = Recipe.objects.all().order_by('-id')
+    return render(request, 'recipes/pages/home.html', {'recipes': recipes},)
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-content.html')
+    return render(request, 'recipes/pages/recipe-content.html',
+                  {
+                      'recipe': make_recipe(),
+                      'is_detail_page': True
+                  })
